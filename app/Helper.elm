e09486b655009d8085bec2d559c161caa9175004
Kai <thiskaiguy@gmail.com>
2017-02-06 18:32:58 +0000

    add a function to calculate the dimensions of the final slices

--- app/Helper.elm
+++ app/Helper.elm
@@ -1,6 +1,7 @@
 module Helper exposing (..)
 
 import Types exposing (..)
+import Preview.Types as Preview
 import Char
 import String
 import Regex exposing (..)
@@ -39,3 +40,78 @@ setSize size name val =
         _ ->
             size
 
+
+getCropDimensions : Size -> Preview.Margins -> CropDimensions
+getCropDimensions size margins =
+    let
+        centerRight =
+            size.width - margins.right
+
+        centerBottom =
+            size.height - margins.bottom
+
+        topLeftCorner =
+            { left = 0
+            , top = 0
+            , right = margins.left
+            , bottom = margins.top
+            }
+
+        topEdge =
+            { left = margins.left
+            , top = 0
+            , right = centerRight
+            , bottom = margins.top
+            }
+
+        topRightCorner =
+            { left = centerRight
+            , top = 0
+            , right = size.width
+            , bottom = margins.top
+            }
+
+        rightEdge =
+            { left = centerRight
+            , top = margins.top
+            , right = size.width
+            , bottom = centerBottom
+            }
+
+        bottomRightCorner =
+            { left = centerRight
+            , top = centerBottom
+            , right = size.width
+            , bottom = size.height
+            }
+
+        bottomEdge =
+            { left = margins.left
+            , top = centerBottom
+            , right = centerRight
+            , bottom = size.height
+            }
+
+        bottomLeftCorner =
+            { left = 0
+            , top = centerBottom
+            , right = margins.left
+            , bottom = size.height
+            }
+
+        leftEdge =
+            { left = 0
+            , top = margins.top
+            , right = margins.left
+            , bottom = centerBottom
+            }
+    in
+        { topLeftCorner = topLeftCorner
+        , topEdge = topEdge
+        , topRightCorner = topRightCorner
+        , rightEdge = rightEdge
+        , bottomRightCorner = bottomRightCorner
+        , bottomEdge = bottomEdge
+        , bottomLeftCorner = bottomLeftCorner
+        , leftEdge = leftEdge
+        }
