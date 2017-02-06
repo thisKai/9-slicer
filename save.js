9362af1a6dc7469d76d60f40bd99b307be495c76
Kai <thiskaiguy@gmail.com>
2017-02-06 22:24:56 +0000

    implement slicing the images and saving
    save in the same location with auto generated names for now

---
+++ save.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const sharp = require('sharp')
+
+function getSliceFilePath(imageUrl, sliceName){
+   const {
+     root,
+     dir,
+     base,
+     ext,
+     name,
+   } = path.parse(imageUrl)
+
+   const slicePath = path.join(dir, `${name}-${sliceName}${ext}`)
+   console.log(slicePath)
+   return slicePath
+}
+function saveSlices(cropData){
+  const {
+    imageUrl,
+    dimensions,
+  } = cropData
+  console.log(getSliceFilePath(imageUrl, 'topLeft'))
+  const image = sharp(imageUrl)
+
+  Object.keys(dimensions)
+    .forEach(sliceName =>
+      image
+        .clone()
+        .extract(dimensions[sliceName])
+        .toFile(getSliceFilePath(imageUrl, sliceName), saveImageCallback)
+    )
+}
+
+
+module.exports = saveSlices
