0c9d3632e0fc16060b09ce57b277fd039993a749
Kai <thiskaiguy@gmail.com>
2017-02-07 15:57:58 +0000

    rename slice saving module

--- ports.js
+++ ports.js
@@ -5,7 +5,7 @@ const {dialog} = require('electron').remote
 const Elm = require('./app.js')
 const {openImageUrl} = require('./sys/file-dialogs.js')
 
-const saveSlices = require('./save.js')
+const saveSlices = require('./save-slices.js')
 
 // start the elm app in the container
 // and keep a reference for communicating with the app
